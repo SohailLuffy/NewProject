f4cbf8b4555e2654f52713a0d4c2188538a3e5a6
SohailLuffy <sohailislammail97@gmail.com>
2022-11-07 09:26:17 +0530

    Add project files.

---
+++ NewProject/Demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject
+{
+    internal class Demo
+    {
+        public int a;
+        public void Test()
+        {
+            Console.WriteLine("Enter a number");
+            this.a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(this.a);
+        }
+
+
+    }
+}
